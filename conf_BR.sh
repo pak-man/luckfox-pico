29ffaae062acbd62b926c3d5075ccde5453dd67e
user <user@email.domain>
2024-04-29 00:07:21 +0200

    Eliminated ubntu toolchain makefiles

--- conf_BR.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cur_dir=$(pwd)
-cd ${SDK_SYSDRV_DIR}/source/buildroot/buildroot # got to buildroot ssource directory
-make luckfox_pico_defconfig
-make menuconfig
-make save_defconfig
-cd $cur_dir
